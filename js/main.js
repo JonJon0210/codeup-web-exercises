aac0bee436e8b11befdb9b2a6f27ed2e1f9c6e21
Jon Cortez <cortez.m.jon@gmail.com>
2020-11-05 16:53:13 -0600

    Becoming a Javascript Wiz

---
+++ js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log("hello, from the javascript console!");
+
+alert("You just won a new ipad.");
+alert("just kidding of course.");
+
+var iPodWinner = "you just won an ipad 5g"
+
+alert(iPodWinner);
+
+var confirmed = confirm('Are you sure you want to do XYZ?');
+console.log(confirmed);
+
+var userInput = prompt('Please type something:');
+console.log('The user entered: ' + userInput);

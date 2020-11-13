2aa0d49f9860b34f57df7d6783e74d59826f057f
Jon Cortez <cortez.m.jon@gmail.com>
2020-11-13 15:49:23 -0600

    completed exercises

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,22 +1,19 @@
-// do {
-//     var oddnum = prompt("Enter odd number")
-//
-// } while (
-//     isNaN (oddnum) || oddnum > 50 || oddnum < 0
-//     )
 
 
-do {
-    var userInput = Number(prompt("Pick an odd number between" +
-        "1 and 50:"));
-    if (userInput % 2 === 0) {
-        alert(userInput + "is not odd, please try again")
-    } else if(userInput > 50) {
-        alert(userInput + "is greater than 50, please try agian")
-    } else if(userInput < 1) {
-        alert(userInput + "is less than 1, please try again")
+while(isNaN(userNum) || (userNum % 2 === 0) || (userNum < 1) || (userNum > 50)) {
+    var userNum = prompt("Please enter an odd number between 1 and 50")
+}
+
+
+console.log("Number to skip is:" + userNum);
+
+for(var i = 1; i < 50; i++) {
+    if (i == userNum) {
+        console.log("Yikes! Skipping number: " + i);
+        continue;
+    } else if (i % 2 === 0) {
+        continue;
     } else {
-        alert("You picked a good one!");
-        break
+        console.log("Here's an odd number: " + i);
     }
-} while (true)
+}

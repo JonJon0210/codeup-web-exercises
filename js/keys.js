0d122525ab21c20ae2316c5a6d43b615e61fcde5
Jon Cortez <cortez.m.jon@gmail.com>
2020-12-10 16:57:12 -0600

    update file

--- js/keys.js
+++
@@ -1,4 +0,0 @@
-//this file is for saving API keys, and NOT uploading to GitHub
-
-let mapBoxToken = 'pk.eyJ1Ijoiam9uam9uMjEwIiwiYSI6ImNraWoweTQ3ZjA5NHkycnBkc3V4aTRtd2wifQ.mpNEytvCgkvdji5oXqwfSA';
-

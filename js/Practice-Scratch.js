b244546b258917f8895ae7270814562469547ec1
Jon Cortez <cortez.m.jon@gmail.com>
2021-01-19 23:43:55 -0600

    scratch practice

--- js/Practice-Scratch.js
+++ js/Practice-Scratch.js
@@ -1,16 +1,16 @@
 "use strict";
 
-for (var i = 0; i <= 100; i++) {
-    if (i % 3 === 0 ) {
-        console.log(i + " Fizz" );
-    } else if (i % 5 === 0 ) {
-        console.log(i + " Buzz");
-    } else if (i % 3 === 0 && i % 5 === 0 ) {
-        console.log(i + " Fizz Buzz");
-    } else {
-        console.log(i);
-    }
-}
+// for (var i = 0; i <= 100; i++) {
+//     if (i % 3 === 0 ) {
+//         console.log(i + " Fizz" );
+//     } else if (i % 5 === 0 ) {
+//         console.log(i + " Buzz");
+//     } else if (i % 3 === 0 && i % 5 === 0 ) {
+//         console.log(i + " Fizz Buzz");
+//     } else {
+//         console.log(i);
+//     }
+// }
 
 // for (var i = 0; i <= 100; i++) {
 //     if (i % 3 === 0 ) {
@@ -25,3 +25,13 @@ for (var i = 0; i <= 100; i++) {
 // }
 
 //jquery
+
+// function sayHello(name) {
+//     return 'Hello, ' + name + '!';
+// }e.log('An error occured!'));
+
+const sayHello = (name) => { 'Hello, ' + name + '!' }
+
+// const sayHello = name => { 'Hello, ' + name + '!' }
+//
+// const sayHello = (name) => `Hello, ${name}!`

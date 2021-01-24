543115726cdb8be841cf05f6606b0988b98e96b2
Jon Cortez <cortez.m.jon@gmail.com>
2021-01-23 20:26:51 -0600

    scratch pad

--- js/Practice-Scratch.js
+++ js/Practice-Scratch.js
@@ -12,17 +12,17 @@
 //     }
 // }
 
-// for (var i = 0; i <= 100; i++) {
-//     if (i % 3 === 0 ) {
-//         console.log(i + " Fizz" );
-//     } else if (i % 5 === 0 ) {
-//         console.log(i + " Buzz");
-//     } else if (i % 3 === 0 && i % 5 === 0 ) {
-//         console.log(i + " Fzz Buzz");
-//     } else {
-//         console.log(i);
-//     }
-// }
+for (var i = 0; i <= 100; i++) {
+    if (i % 3 === 0 ) {
+        console.log(i + " Fizz" );
+    } else if (i % 5 === 0 ) {
+        console.log(i + " Buzz");
+    } else if (i % 3 === 0 && i % 5 === 0 ) {
+        console.log(i + " Fzz Buzz");
+    } else {
+        console.log(i);
+    }
+}
 
 //jquery
 
